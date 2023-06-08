9b49951c7674e212d11fc8ab80f098ce31a26e50
zzh <1746556951@qq.com>
2023-06-08 13:32:54 +0800

    chore: 配置babel自动提那几react运行时代码

--- projects/browser-extension-example/.babelrc.js
+++ projects/browser-extension-example/.babelrc.js
@@ -24,6 +24,10 @@ module.exports = function (api) {
                 'icons'
             ]
         ],
-        presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript']
+        presets: [
+            '@babel/preset-env',
+            ['@babel/preset-react', { runtime: 'automatic' }],
+            '@babel/preset-typescript'
+        ]
     }
 }
